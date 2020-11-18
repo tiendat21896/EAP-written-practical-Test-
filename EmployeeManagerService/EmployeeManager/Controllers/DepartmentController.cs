c09e419f2b8ec230b36ad2b0943cfc31247fa37f
tiendat21896 <tiendat21896@gmail.com>
2020-11-18 10:48:34 +0700

    up

---
+++ EmployeeManagerService/EmployeeManager/Controllers/DepartmentController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeConsumer.Models;
+
+namespace EmployeeConsumer.Controllers
+{
+
+    public class DepartmentController : Controller
+    {
+        ServiceClient servicesClient = new ServicesClient();
+        // GET: Department
+        public ViewResult Index(string sortOrder, string search, string currentFilter, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            /*            ViewBag.listLocation = servicesClient.getAllLocations();
+            */
+            if (search != null)
+            {
+                page = 1; // nếu search có giá trị trả về page = 1
+            }
+            else
+            {
+                search = currentFilter; //  nếu có thì render phần dữ liệu search ra
+            }
+            ViewBag.CurrentFilter = search;
+            var departments = from s in servicesClient.getAllDepartment() select s;
+            if (!String.IsNullOrEmpty(search)) // check nếu search string có thì in ra hoặc không thì không in ra
+            {
+                departments = departments.Where(s => s.DepartmentName.Contains(search) || s.DepartmentName.Contains(search)); // contains là để check xem lastname hoặc firstName có chứa search string ở trên 
+            }
+            switch (sortOrder)
+            {
+                case "name desc":
+                    departments = departments.OrderByDescending(s => s.DepartmentName); // các case tương đương với các cột muốn sort
+                    break;
+
+                default:
+                    departments = departments.OrderBy(s => s.DepartmentName);
+                    break;
+            }
+
+            return View(departments.ToList());
+
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int id)
+        {
+            var department = servicesClient.getAllDepartment().Where(b => b.DepartmentID == id).FirstOrDefault();
+            return View(department);
+
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Department newDepartment)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    servicesClient.AddDepartment(newDepartment);
+                    return RedirectToAction("Index", "Department");
+                }
+
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeManagerService/EmployeeManagerService/EmployeeManager.svc.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeManagerService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeManager" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeManager.svc or EmployeeManager.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeManager : IEmployeeManager
+    {
+        EmployeeManagerDataContext data = new EmployeeManagerDataContext();
+
+        public bool AddEmployee(Employee employee)
+        {
+            try
+            {
+                data.Employees.InsertOnSubmit(employee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+
+        }
+
+        public bool AddDepartment(Department department)
+        {
+            try
+            {
+                data.Departments.InsertOnSubmit(department);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool DeleteEmployee(string id)
+        {
+            try
+            {
+                var Employee = data.Employees.Where(ca => ca.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(Employee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool DeleteDepartment(string id)
+        {
+            try
+            {
+                var Department = data.Departments.Where(ca => ca.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(Department);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditEmployee(string id, Employee newEmployee)
+        {
+            try
+            {
+                var Employee = data.Employees.Where(ca => ca.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(Employee);
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditDepartment(string id, Department newDepartment)
+        {
+            try
+            {
+                var Department = data.Departments.Where(ca => ca.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(Department);
+                data.Departments.InsertOnSubmit(newDepartment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            try
+            {
+                var Employees = (from Employee in data.Employees select Employee).ToList();
+                return Employees;
+            }
+            catch { return null; }
+        }
+
+        public List<Department> GetDepartment()
+        {
+            try
+            {
+                var Departments = (from Department in data.Departments select Department).ToList();
+                return Departments;
+            }
+            catch { return null; }
+        }
+
+        public List<Department> GetDepartments()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
